09de5cdc856a5c0183bffedfd39216bd749e26d3
shubh-37 <shubh.arya37@gmail.com>
2022-07-12 23:40:59 +0530

    added bcrypt middleware in the schema

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -21,8 +21,13 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required : [true, 'Please provide a password'],
         minlength: 6,
-        maxlength: 12,
     }
+});
+
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
