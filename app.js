295a377d1502c754e28837f743553c3d40bd4f58
shubh-37 <shubh.arya37@gmail.com>
2022-07-14 23:35:04 +0530

    imported the authentication middleware and used in jobs route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 //connectDB
 const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
 
 //routers
 const authRouter = require('./routes/auth');
@@ -18,8 +19,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.use('/api/v1/auth',authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
