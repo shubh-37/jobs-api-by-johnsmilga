9398267beea21a8f3ee447558eef83ebbb38d86e
shubh-37 <shubh.arya37@gmail.com>
2022-07-14 17:08:44 +0530

    added functionality to register and login controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,36 @@
 const User = require('../models/User');
 const {StatusCodes} = require('http-status-codes');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 const jwt = require('jsonwebtoken');
 
 
 const register = async (req,res) => {
-    const user = await User.create({ ...req.body })
-    const token = jwt.sign({userId: user._id, name: user.name}, 'jwtSecret', {
-        expiresIn: '30d',
-    });
-    
-    res.status(StatusCodes.CREATED).json({user : { name: user.name}, token });
+    const user = await User.create({ ...req.body });
+    const token = user.createJWT();
+    res
+    .status(StatusCodes.CREATED)
+    .json({user : { name: user.name }, token });
 }
 
 const login = async (req,res) =>{
-    res.send('Login User')
+    const { email, password } = req.body
+
+    if(!email || !password){
+        throw new BadRequestError('Please provide both email and password!')
+    }
+    const user = await User.findOne({ email })
+    
+    if(!user){
+        throw new UnauthenticatedError('Invalid credentials!')
+    }
+    //compare password
+    const isPasswordCorrect = await user.comparePassword(password);
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid credentials!')
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 module.exports = {
